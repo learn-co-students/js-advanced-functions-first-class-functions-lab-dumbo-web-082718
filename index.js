37539ef39b497e64b208882cf193afb83dd24cbc
GavIn Sokolof <sokolofg@gmail.com>
2018-10-08 15:53:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+  newArr = [];
+  newArr.push(array[0], array[1]);
+  return newArr;
+};// Code your solution in this file!
+
+const returnLastTwoDrivers = function(array){
+  newArr = [];
+  lastIndex = array.length - 1;
+  secondToLast = array.length -2;
+  newArr.push(array[secondToLast], array[lastIndex]);
+  return newArr;
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(num) {
+  return function mp(fare) {
+    return num * fare;
+  }
+}
+
+const fareDoubler = function mp(fare) {
+    return fare * 2;
+  }
+
+  const fareTripler = function mp(fare) {
+      return fare * 3;
+    }
+
+function fetchSpecifiedDrivers(array, returnFirstTwoDrivers){
+  return returnFirstTwoDrivers(array);
+}
